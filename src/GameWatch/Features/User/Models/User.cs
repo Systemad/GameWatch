62987daf873c0f530e38a39dc52f6c9553fc5c7c
Systemad <dannesuke@gmail.com>
2023-02-23 01:34:39 +0100

    chore: final fixes for authentication, firstsetup of Mudblazor layout

--- src/GameWatch/Features/User/Models/User.cs
+++ src/GameWatch/Features/User/Models/User.cs
@@ -1,8 +1,12 @@
-﻿namespace GameWatch.Features.User.Models;
+﻿using GameWatch.Features.Wishlist.Models;
+
+namespace GameWatch.Features.User.Models;
 
 public class User
 {
     public string UserId { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
+
+    public List<WishlistItem> WishlistItems { get; set; }
 }
---
+++ src/GameWatch/Features/Wishlist/Models/WishlistItem.cs
@@ -0,0 +1,9 @@
+﻿namespace GameWatch.Features.Wishlist.Models;
+using GameWatchUser = User.Models.User;
+
+public class WishlistItem
+{
+    public string GameId { get; set; }
+    public string UserId { get; set; }
+    public GameWatchUser User { get; set; }
+}
